a1475ebe1a6c755cd7990ef9fbfea21e8609a196
신선영 <sunyeong0412@naver.com>
2022-10-21 01:33:41 +0900

    [#S07P31A102-83] feat: add background images

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,11 @@
+import LoginForm from '../components/LoginForm';
+import Image1 from '../assets/background1.png';
+
 function Login() {
   return (
-    <div className="Login">
+    <div className="Login" style={{ backgroundImage: `url(${Image1})` }}>
       <h1>Login</h1>
+      <LoginForm />
     </div>
   );
 }
