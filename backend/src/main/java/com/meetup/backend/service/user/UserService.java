4736217e6a583bc5c4164e64e90e956712e5c41f
박성민 <psm7335@naver.com>
2022-10-23 02:52:17 +0900

    [#S07P31A102-30] feat: login service

--- backend/src/main/java/com/meetup/backend/service/user/UserService.java
+++ backend/src/main/java/com/meetup/backend/service/user/UserService.java
@@ -1,4 +1,11 @@
 package com.meetup.backend.service.user;
 
+import com.meetup.backend.dto.user.LoginRequestDto;
+import com.meetup.backend.dto.user.LoginResponseDto;
+
+/**
+ * created by seongmin on 2022/10/23
+ */
 public interface UserService {
+    LoginResponseDto login(LoginRequestDto requestDto);
 }
