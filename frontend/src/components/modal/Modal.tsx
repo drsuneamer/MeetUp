30fb000ad6deb6a9779d558084bc5b7b3a97a052
MinjinChae <eominjin25@gmail.com>
2022-10-23 17:17:23 +0900

    [#S07P31A102-85][FE] feat: add modal logic

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,9 +1,22 @@
-function Modal() {
+import React from 'react';
+interface props {
+  isOpen: boolean;
+  isClose: () => void; // 함수 타입 정의
+}
+const Modal = (props: props): React.ReactElement => {
+  const { isOpen, isClose } = props;
+
   return (
-    <div className="Modal">
-      <h1>Modal</h1>
+    <div>
+      <div className={isOpen ? 'bg' : ''} />
+      <div className={isOpen ? 'modal active' : 'modal'}>
+        {isOpen ? (
+          <div>
+             <button className="close" onClick={isClose}> x </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
-}
-
+};
 export default Modal;
--- frontend/src/pages/CreateChannel.tsx
+++ frontend/src/pages/CreateChannel.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import Modal from '../components/modal/Modal';
+
 function CreateChannel() {
+  const [visibleModal, setVisibleModal] = useState<boolean>(false)
+  
+  function open() {
+    setVisibleModal(!visibleModal);
+  }
+  
+  function onClose() {
+    setVisibleModal(!visibleModal);
+  }
   return (
     <div className="CreateChannel">
       <h1>CreateChannel</h1>
+      <button className='bg-title hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={open}>Show Modal</button>
+      <Modal isOpen={visibleModal} isClose={onClose}></Modal>
     </div>
   );
 }
