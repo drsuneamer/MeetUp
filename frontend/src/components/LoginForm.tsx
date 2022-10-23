39f288635f0cdd268dc2b67d0b45fe33418bffca
신선영 <sunyeong0412@naver.com>
2022-10-23 21:37:27 +0900

    [#S07P31A102-87] feat: basic layout for login

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
+
 function LoginForm() {
+  const [id, setID] = useState('');
+  const [pw, setPW] = useState('');
+
+  const onChangeID = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setID(e.target.value);
+  };
+
+  const onChangePW = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPW(e.target.value);
+  };
+
+  const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const login: object = { idValue: id, pwValue: pw };
+    console.log(login);
+  };
   return (
-    <div className="LoginForm">
-      <h1>LoginForm</h1>
+    // 전체
+    <div className="flex h-[80vh] w-[80vw]">
+      {/* 입력창 */}
+      <div className="bg-background flex flex-col items-center justify-center rounded-l-lg">
+        <input
+          onChange={onChangeID}
+          type="text"
+          placeholder="Mattermost ID"
+          className="w-3/5 text-center border-b-2 border-b-title py-1 px-2 focus:outline-none"
+        />
+        <input
+          onChange={onChangePW}
+          type="password"
+          placeholder="Mattermost PW"
+          className="w-3/5 text-center border-b-2 border-b-title py-1 px-2 focus:outline-none"
+        />
+        <button onClick={onSubmit} className="bg-title rounded drop-shadow w-s h-s text-background">
+          매터모스트로 로그인
+        </button>
+      </div>
+      {/* 타이틀 */}
+      <div className="bg-title grid place-items-center rounded-r-lg">
+        <div className="font-damion text-background text-2xl">MeetUp</div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,10 @@
 import LoginForm from '../components/LoginForm';
 import Image1 from '../assets/background1.png';
+// import Image2 from '../assets/background2.png';
 
 function Login() {
   return (
-    <div className="Login" style={{ backgroundImage: `url(${Image1})` }}>
-      <h1>Login</h1>
+    <div className="h-screen bg-cover flex items-center justify-center" style={{ backgroundImage: `url(${Image1})` }}>
       <LoginForm />
     </div>
   );
