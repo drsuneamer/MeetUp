99330879f602233ca1e0b5d22c0cee92385c2212
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-10-23 20:39:42 +0900

    [#S07P31A102-86][FE] feat: set schedule reducer

--- frontend/src/stores/ConfigStore.ts
+++ frontend/src/stores/ConfigStore.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import calendarReducer from './calendar';
+import scheduleReducer from './schedule';
 
 export const store = configureStore({
   reducer: {
     calendar: calendarReducer,
+    schedule: scheduleReducer,
   },
 });
 
---
+++ frontend/src/stores/schedule.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { tSchedule, tScheduleDetail } from '../../index';
+import { RootState } from './ConfigStore';
+
+const initialState: tSchedule = {
+  '2022-01-01': [
+    {
+      start: { hour: 1, minute: 20 },
+      end: { hour: 1, minute: 40 },
+      color: 'pink',
+      title: '코딩하기',
+    },
+  ],
+};
+
+export const scheduleSlice = createSlice({
+  name: 'schedule',
+  initialState,
+  reducers: {
+    addSchedule: (state, action: PayloadAction<{ date: string; data: tScheduleDetail }>) => {
+      if (!state[action.payload.date]) {
+        state[action.payload.date] = [];
+      }
+      state[action.payload.date] = [...state[action.payload.date], action.payload.data];
+    },
+    removeSchedule: (state, action: PayloadAction<{ date: string; index: number }>) => {
+      delete state[action.payload.date][action.payload.index];
+    },
+  },
+});
+
+export const { addSchedule, removeSchedule } = scheduleSlice.actions;
+export const schedules = (state: RootState) => state.schedule;
+
+export default scheduleSlice.reducer;
