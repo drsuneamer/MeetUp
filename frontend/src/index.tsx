74a1bb4fcb84c3f9fa3e71cc5d88cf8922a680cc
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-10-23 21:00:01 +0900

    [#][FE] chore: redux setting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './stores/ConfigStore';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
