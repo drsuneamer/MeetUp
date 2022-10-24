e7239dbfe6879602ab72c2b4ba9327fd9ef23585
신선영 <sunyeong0412@naver.com>
2022-10-24 17:49:55 +0900

    [#S07P31A102-155][FE] feat: change css options for layout

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import Header from './Header';
 import Footer from './Footer';
 
 function Layout(props: { children: React.ReactNode }) {
-  if (window.location.pathname === '/login' || window.location.pathname === '/*')
+  if (window.location.pathname === '/login')
     return (
       <div className="">
         <main>{props.children}</main>
@@ -12,7 +12,7 @@ function Layout(props: { children: React.ReactNode }) {
     return (
       <div className="">
         <Header />
-        <main className="pt-[60px] min-h-[calc(100vh-60px)]">{props.children}</main>
+        <main className="min-h-[calc(100vh-60px)]">{props.children}</main>
         {/* padding-top: header와 겹치지 않기 위함 */}
         {/* min-height: 100vh - (헤더/푸터 크기) -> footer 하단 고정 위해 사용 */}
         <Footer />
