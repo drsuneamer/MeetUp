dfc288e081f5b20cf94f0c5bf3480f881c4534bc
MinjinChae <eominjin25@gmail.com>
2022-10-27 08:52:23 +0900

    [#S07P31A102-78][FE] feat: change dropdown logic

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,16 +1,32 @@
-import React, { useState, PropsWithChildren }from 'react';
-import SearchableDropdown from "../SearchableDropdown";
+import  { PropsWithChildren }from 'react';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 interface ModalDefaultType {
   onClickToggleModal: () => void;  // 함수 타입 정의
 }
 
+const top100Films = [
+  { label: 'The Shawshank Redemption' },
+  { label: 'The Godfather' },
+  { label: 'The Godfather: Part II' },
+  { label: 'The Dark Knight' },
+  { label: '12 Angry Men' },
+  { label: "Schindler's List" },
+  { label: 'Pulp Fiction' },
+  { label: 'Aliens' },
+  { label: 'Oldboy' },
+  { label: 'Once Upon a Time in America' },
+  { label: 'Witness for the Prosecution' },
+  { label: 'Das Boot' },
+  { label: 'Citizen Kane' },
+  { label: 'North by Northwest' },
+  { label: 'Vertigo' },
+];
 function Modal({
   onClickToggleModal,
 }: PropsWithChildren<ModalDefaultType>) {
-  const [value, setValue] = useState("Select option...");
-  const animals = ['a', 'b', 'c']
-  const id=1
   return (
     <div className="w-[100%] h-[100%] fixed flex justify-center items-center">
       <div className="w-[600px] h-[600px] flex flex-col items-center bg-background z-10 rounded drop-shadow-shadow">
@@ -34,17 +50,18 @@ function Modal({
           </div>
           <div>
             <span>알림 보낼 채널</span>
-            {/* <SearchableDropdown
-              options={animals}
-              label="name"
-              id={id}
-              selectedVal={value}
-              handleChange={(val:any) => setValue(val)}/> */}
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={top100Films}
+              sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="채널 선택하기" />}
+            />
           </div>
-          <div>
+          {/* <div>
             <span>팀원 초대하기</span>
                  
-          </div>
+          </div> */}
           <button>밋업 등록하기</button>
         </div>
         </div>
