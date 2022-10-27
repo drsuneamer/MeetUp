48498351f02437422996d564e4e32a907f350eac
gyumin.q.lee <gyumin.q.lee@gmail.com>
2022-10-27 16:02:57 +0900

    [#][FE] fix: tailwind css error fix

--- frontend/src/components/ChannelList.tsx
+++ frontend/src/components/ChannelList.tsx
@@ -30,7 +30,7 @@ function ChannelList() {
         <Link to="/create-channel" className="flex flex-col justify-center">
             {/* plus button */}
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-7 h-7 text-point">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-11.25a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z" clip-rule="evenodd" />
+              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-11.25a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z" clipRule="evenodd" />
             </svg>
         </Link>
       </div>
--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   };
 
   return (
-    <div className="fixed flex items-center justify-between bg-[white] w-full h-l border-b-2 border-line">
+    <div className="fixed flex items-center justify-between bg-[white] w-full h-l border-b-2 border-line z-50">
       <div>
         <Link to="/">
           <img className="h-s ml-2" src={LogoImage} alt="logo" />
@@ -20,14 +20,14 @@ function Header() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="w-6 h-6 pt-0.5 cursor-pointer"
           onClick={logout}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
           />
         </svg>
