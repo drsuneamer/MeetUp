562babb25615cc9f2de6e860de36f59f595361bc
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-10-27 17:32:52 +0900

    [#S07P31A102-147] feat: Scheudule Service test 작성

---
+++ backend/src/test/java/com/meetup/backend/service/meeting/ScheduleServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.meetup.backend.service.meeting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleServiceImplTest {
+
+    @Test
+    void getScheduleResponseDtoById() {
+    }
+
+    @Test
+    void getScheduleResponseDtoByUserAndDate() {
+    }
+
+    @Test
+    void createSchedule() {
+    }
+
+    @Test
+    void updateSchedule() {
+    }
+
+    @Test
+    void deleteSchedule() {
+    }
+}
