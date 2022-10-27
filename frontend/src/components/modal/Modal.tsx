accf84824ab0a90eb1e1008fd828ca054a7d8c73
MinjinChae <eominjin25@gmail.com>
2022-10-27 13:08:45 +0900

    [#S07P31A102-78][FE] design: create modal layout

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -2,7 +2,6 @@ import  { PropsWithChildren }from 'react';
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-// import Stack from '@mui/material/Stack';
 
 interface ModalDefaultType {
   onClickToggleModal: () => void;  // 함수 타입 정의
@@ -12,93 +11,73 @@ interface ChannelOptionType {
   title: string;
 }
 
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
+const channels = [
+  { title: '서울_1반_팀장채널'},
+  { title: 'A101' },
+  { title: 'A102' },
+  { title: 'A103' },
+  { title: 'A104' },
+  { title: 'A105' },
+  { title: 'A106' },
+  { title: 'A107' },
+  { title: 'A102_scrum' },
+  { title: 'A102_jira_bot' },
 ];
 function Modal({
   onClickToggleModal,
 }: PropsWithChildren<ModalDefaultType>) {
   const defaultProps = {
-    options: top100Films,
+    options: channels,
     getOptionLabel: (option: ChannelOptionType) => option.title,
   };
   const flatProps = {
-    options: top100Films.map((option) => option.title),
+    options: channels.map((option) => option.title),
   };
   const [value, setValue] = React.useState<ChannelOptionType | null>(null);
-  
+  // const ELEMENT_TO_SHOW = 4;
   return (
     <div className="w-[100%] h-[100%] fixed flex justify-center items-center">
       <div className="w-[600px] h-[600px] flex flex-col items-center bg-background z-10 rounded drop-shadow-shadow">
-        <svg onClick={onClickToggleModal} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 stroke-title">
+        <svg onClick={onClickToggleModal} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" className="w-6 h-6 stroke-title mt-[15px] ml-[550px] cursor-pointer">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg>
         <div>
-          <div>
-            <div>미팅명</div>
-            <input type="text" name="title" className="w-s h-[40px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
+          <div className="mt-[20px]">
+            <div className="text-title font-bold">미팅명</div>
+            <input type="text" name="title" className="w-[450px] h-[30px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
           </div>
-          <div>
-            <div>날짜</div>
-            <input type="text" name="title" className="w-s h-[40px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
+          <div className="mt-[20px]">
+            <div className="text-title font-bold">날짜</div>
+            <input type="text" name="title" className="w-[450px] h-[30px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
           </div>
-          <div>
-            <div>시간</div>
-            <input type="text" name="title" className="w-s h-[40px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
+          <div className="mt-[20px]">
+            <div className="text-title font-bold">시간</div>
+            <input type="text" name="title" className="w-[450px] h-[30px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
           </div>
-          <div>
-            <div>내용</div>
-            <input type="text" name="title" className="w-s h-[40px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
+          <div className="mt-[20px]">
+            <div className="text-title font-bold">내용</div>
+            <input type="text" name="title" className="w-[450px] h-[30px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point"/>
           </div>
-          <div>
-            <div>알림 보낼 채널</div>
-            {/* <Stack spacing={1} sx={{ width: 300 }}> */}
+          <div className="mt-[20px]">
+            <div className="text-title font-bold">알림 보낼 채널</div>
             <Autocomplete
-              className="w-s hover:"
+              className="w-[450px]"
+              ListboxProps={{ style: { maxHeight: '150px' } }}
               {...defaultProps}
-              id="disable-close-on-select"
-              disableCloseOnSelect
+              id="select-channel"
+              // options={channels.filter((el, i) => {  // here add a filter for options
+              //   if (i < ELEMENT_TO_SHOW) return el;
+              // })}
               renderInput={(params) => (
                 <TextField {...params} label="채널 선택하기" variant="standard" />
               )}
             />
-            {/* </Stack> */}
           </div>
           {/* <div>
             <span>팀원 초대하기</span>
                  
           </div> */}
-          <button className="bg-title hover:bg-hover text-background py-2 px-4 rounded w-s h-s">밋업 등록하기</button>
+          <button className="bg-title hover:bg-hover text-background mt-[50px] rounded w-[450px] h-s">밋업 등록하기</button>
         </div>
         </div>
       <div
