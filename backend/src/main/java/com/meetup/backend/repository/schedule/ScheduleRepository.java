ace460b89d7a34c3f0d9c276445bcdadfa8474da
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-10-27 17:31:58 +0900

    [#S07P31A102-147] feat: Scheudule Repository 메소드 추가 (유저&기간별추가)

--- backend/src/main/java/com/meetup/backend/repository/schedule/ScheduleRepository.java
+++ backend/src/main/java/com/meetup/backend/repository/schedule/ScheduleRepository.java
@@ -1,11 +1,17 @@
 package com.meetup.backend.repository.schedule;
 
 import com.meetup.backend.entity.schedule.Schedule;
+import com.meetup.backend.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * created by myeongseok on 2022/10/25
  */
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-
+    @Query("SELECT s FROM Schedule s WHERE s.user = :user AND s.type = 'schedule' AND s.start BETWEEN :from AND :to ")
+    List<Schedule> findAllByStartBetweenAndUser(LocalDateTime from, LocalDateTime to, User user);
 }
