4d015d21cb0c94b6b2e3fe1c4c8fc11e14cc9061
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-10-27 17:32:43 +0900

    [#S07P31A102-147] feat: Scheudule Controller 메소드 추가

---
+++ backend/src/main/java/com/meetup/backend/controller/ScheduleController.java
@@ -0,0 +1,52 @@
+package com.meetup.backend.controller;
+
+import com.meetup.backend.dto.Schedule.ScheduleResponseDto;
+import com.meetup.backend.dto.Schedule.ScheduleUpdateRequestDto;
+import com.meetup.backend.service.auth.AuthService;
+import com.meetup.backend.service.meeting.ScheduleService;
+import com.meetup.backend.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * created by myeongseok on 2022/10/23
+ */
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/schedule")
+public class ScheduleController {
+    private final ScheduleService scheduleService;
+
+    private final UserService userService;
+
+    private final AuthService authService;
+
+    // 스케쥴id로 단일 스케쥴 조회
+    @GetMapping("/{scheduleId}")
+    public ResponseEntity<?> getSchedule(@PathVariable("scheduleId") Long scheduleId) {
+        log.info("scheduleId = {}", scheduleId);
+
+        String userId = authService.getMyInfoSecret().getId();
+        ScheduleResponseDto scheduleResponseDto = scheduleService.getScheduleResponseDtoById(userId, scheduleId);
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleResponseDto);
+
+    }
+
+    // 스케쥴 id 해당되는 스케쥴 수정
+    @PatchMapping
+    public ResponseEntity<?> updateSchedule(@RequestBody @Valid ScheduleUpdateRequestDto scheduleUpdateRequestDto) {
+        log.info("scheduleUpdateRequestDto : {}", scheduleUpdateRequestDto);
+        String userId = authService.getMyInfoSecret().getId();
+        scheduleService.updateSchedule(userId, scheduleUpdateRequestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}
