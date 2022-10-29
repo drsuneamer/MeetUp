84135f1a6ffdb8857d9c13d1ebb4c02591df1ae8
MinjinChae <eominjin25@gmail.com>
2022-10-29 18:18:23 +0900

    [#S07P31A102-85][FE] design: edit margin

--- frontend/src/components/modal/CreateChannelModal.tsx
+++ frontend/src/components/modal/CreateChannelModal.tsx
@@ -45,7 +45,7 @@ function CreateChannelModal({
 </svg>
         <div>
           <p className="text-placeholder text-[15px]">mm에 새로운 팀 채널 생성하기</p>
-          <div className="mt-[35px]">
+          <div className="mt-[25px]">
             <div className="text-title font-bold text-[20px] mb-[10px]">공개여부<span className="text-cancel">&#42;</span></div>
                 <div className="flex items-center mb-[5px]">
                     <input id="default-radio-1" type="radio" value="public" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:blue-500" />
@@ -58,8 +58,8 @@ function CreateChannelModal({
                     <span className="ml-[12px] text-placeholder text-[14px]">초대받은 사람만 이 채널에 참여할 수 있습니다</span>
                 </div>
           </div>
-          <div className="mt-[20px]">
-            <div className="text-title font-bold">팀</div>
+          <div className="mt-[25px]">
+            <div className="text-title font-bold text-[20px]">팀<span className="text-cancel">&#42;</span></div>
             <Autocomplete
               className="w-[450px]"
               ListboxProps={{ style: { maxHeight: '150px' } }}
@@ -73,18 +73,18 @@ function CreateChannelModal({
               )}
             />
           </div>
-          <div className="mt-[35px]">
+          <div className="mt-[25px]">
             <div className="text-title font-bold text-[20px]">채널이름<span className="text-cancel">&#42;</span></div>
             <input type="text" name="title" className="w-[450px] h-[30px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point text-[16px]"/>
           </div>
-          <div className="mt-[35px]">
+          <div className="mt-[25px]">
             <div className="text-title font-bold text-[20px]">채널 URL<span className="text-cancel">&#42;</span></div>
             <input type="text" name="title" className="w-[450px] h-[30px] outline-none border-solid border-b-2 border-title focus:border-b-point active:border-b-point text-[16px]"/>
           </div>
           {/* <div className="mt-[20px]">
             <div className="text-title font-bold">팀원 초대하기</div>
           </div> */}
-          <button className="font-bold bg-title hover:bg-hover text-background mt-[70px] rounded w-[450px] h-s drop-shadow-button">팀 채널 생성하기</button>
+          <button className="font-bold bg-title hover:bg-hover text-background mt-[50px] rounded w-[450px] h-s drop-shadow-button">팀 채널 생성하기</button>
         </div>
         </div>
       <div
