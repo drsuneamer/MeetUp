8363f9f7726477d84e918a38605d32aee215f5df
신선영 <sunyeong0412@naver.com>
2022-10-31 01:06:06 +0900

    [#S07P31A102-105][FE] feat: 팀/채널 목록 API(GET) 설정 - 드롭다운 설정 수정

--- frontend/src/pages/CreateChannel.tsx
+++ frontend/src/pages/CreateChannel.tsx
@@ -4,31 +4,55 @@ import axios from 'axios';
 import ColorPicker from 'react-pick-color';
 import Layout from '../components/layout/Layout';
 import MultipleLevelSelection from '../components/MultipleLevelSelection';
+import { useSelector } from 'react-redux';
 
 interface Category {
-  categoryId: string;
-  parentId: string;
-  name: string;
+  id: string;
+  teamId?: string;
+  displayName: string;
 }
 
 function CreateChannel() {
+  const t = useSelector((state: any) => state.teamNum.value).id;
   // const navigate = useNavigate();
 
-  const getChannel = async () => {
-    await axios
-      .get('http://localhost:8080/api/meetup/sync', {
+  const [lv1Categories, setLv1] = useState<any>([]);
+  const [lv2Categories, setLv2] = useState<any>([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/meetup/team', {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem('accessToken')}`,
+        },
+      })
+      .then((res) => {
+        setLv1(res.data);
+        console.log(res.data);
+      });
+  }, []);
+  console.log(lv1Categories);
+
+  if (lv1Categories.length > 0) {
+    if (lv1Categories[0].teamId === undefined) {
+      for (let i = 0; i < lv1Categories.length; i++) {
+        lv1Categories[i].teamId = '0';
+      }
+    }
+  }
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/meetup/channel/${t}`, {
         headers: {
           Authorization: `Bearer ${window.localStorage.getItem('accessToken')}`,
         },
       })
       .then((res) => {
         console.log(res);
+        setLv2(res.data);
       });
-  };
-
-  useEffect(() => {
-    getChannel();
-  });
+  }, [t]);
 
   // 알림을 받을 채널 선택하기
   const [category, setCategory] = useState<Category>();
@@ -37,34 +61,25 @@ function CreateChannel() {
 
   useEffect(() => {
     if (category !== undefined) {
-      setChannelId(category.categoryId);
+      setChannelId(category.id);
     }
   }, [category]);
 
-  const lv1Categories = [
-    //팀
-    { categoryId: '1', name: '1반', parentId: '0' },
-    { categoryId: '2', name: '2반', parentId: '0' },
-    { categoryId: '3', name: '3반', parentId: '0' },
-    { categoryId: '4', name: '4반', parentId: '0' },
-    { categoryId: '9', name: '9반', parentId: '0' },
-  ];
-
-  const lv2Categories = [
-    //채널 (parentId에 따라 팀에 종속됨)
-    { categoryId: '1', name: '101', parentId: '1' },
-    { categoryId: '2', name: '201', parentId: '1' },
-    { categoryId: '3', name: '202', parentId: '2' },
-    { categoryId: '4', name: '203', parentId: '2' },
-    { categoryId: '5', name: '204', parentId: '2' },
-    { categoryId: '6', name: '205', parentId: '2' },
-    { categoryId: '7', name: '206', parentId: '2' },
-    { categoryId: '8', name: '207', parentId: '2' },
-    { categoryId: '9', name: '208', parentId: '2' },
-  ];
+  // const lv2Categories = [
+  //   //채널 (teamId에 따라 팀에 종속됨)
+  //   { id: '1', displayName: '101', teamId: '84s3g1pdttg1bjjo1s5fhrdf1c' },
+  //   { id: '2', displayName: '201', teamId: '84s3g1pdttg1bjjo1s5fhrdf1c' },
+  //   { id: '3', displayName: '202', teamId: '84s3g1pdttg1bjjo1s5fhrdf1c' },
+  //   { id: '4', displayName: '203', teamId: '84s3g1pdttg1bjjo1s5fhrdf1c' },
+  //   { id: '5', displayName: '204', teamId: 'hgpfx9pqxiyij8zt9qz9tk8yga' },
+  //   { id: '6', displayName: '205', teamId: 'hgpfx9pqxiyij8zt9qz9tk8yga' },
+  //   { id: '7', displayName: '206', teamId: 'hgpfx9pqxiyij8zt9qz9tk8yga' },
+  //   { id: '8', displayName: '207', teamId: 'hgpfx9pqxiyij8zt9qz9tk8yga' },
+  //   { id: '9', displayName: '208', teamId: 'hgpfx9pqxiyij8zt9qz9tk8yga' },
+  // ];
 
   const categories: Category[] = [...lv1Categories, ...lv2Categories];
-  const categoriesByParentId = (parentId: string | number) => categories.filter((category) => category.parentId === `${parentId}`);
+  const categoriesByteamId = (teamId: string | number) => categories.filter((category) => category.teamId === `${teamId}`);
 
   // MM 채널 이름 설정
   const [title, setTitle] = useState('');
@@ -101,65 +116,72 @@ function CreateChannel() {
   //       console.log(res);
   //     });
   // };
-
-  return (
-    <Layout>
-      <div className="text-m mx-[20vw] pt-[20vh] pb-[180px]">
-        <div className="mb-10 z-30">
-          <div className="font-bold text-title">알림을 받을 채널 선택하기</div>
-          <div className="">
-            <MultipleLevelSelection
-              initialItems={categoriesByParentId(0)}
-              getItemKey={(item) => item.categoryId}
-              getItemLabel={(item) => item.name}
-              getNestedItems={(item) => categoriesByParentId(item.categoryId)}
-              hasNestedItems={(_, level) => level < 2}
-              isEqual={(item, item2) => item?.categoryId === item2?.categoryId}
-              placeholder="내 MM의 모든 팀/채널 확인하기"
-              onChange={setCategory}
-            />
-          </div>
-        </div>
-
-        <div className="mb-5">
-          <div className="font-bold text-title">MM 채널 이름 (변경 가능)</div>
-          <div className="flex justify-center">
-            <input
-              onChange={titleChange}
-              type="text"
-              placeholder="ex. 서울_1반_A102"
-              className="w-full text-center placeholder-placeholder border-b-2 border-b-title py-1 px-2 mb-10 placeholder:text-s focus:outline-none focus:border-b-footer"
-            />
-          </div>
-        </div>
-
-        <div className="z-30">
-          <div className="font-bold text-title">달력에 표시할 색상 선택</div>
-          <div className="flex mt-2 mb-12">
-            {/* 색상 선택 */}
-            <div onClick={openColor} style={{ backgroundColor: `${color}` }} className="rounded-full m-[5px] w-[30px] h-[30px] cursor-pointer"></div>
-            {open ? (
-              <div onClick={openColor} className="absolute z-30">
-                <ColorPicker color={color} onChange={(color) => setColor(color.hex)} />
-              </div>
-            ) : (
-              <div></div>
-            )}
-            <div onClick={openColor} className="text-xs text-label pt-[9px] ml-1.5 cursor-pointer">
-              {color}
+  if (lv1Categories.length > 0) {
+    return (
+      <Layout>
+        <div className="text-m mx-[20vw] pt-[20vh] pb-[180px]">
+          <div className="mb-10 z-30">
+            <div className="font-bold text-title">알림을 받을 채널 선택하기</div>
+            <div className="">
+              <MultipleLevelSelection
+                initialItems={categoriesByteamId(0)}
+                getItemKey={(item) => item.id}
+                getItemLabel={(item) => item.displayName}
+                getNestedItems={(item) => categoriesByteamId(item.id)}
+                hasNestedItems={(_, level) => level < 2}
+                isEqual={(item, item2) => item?.id === item2?.id}
+                placeholder="내 MM의 모든 팀/채널 확인하기"
+                onChange={setCategory}
+              />
             </div>
           </div>
-        </div>
 
-        <button
-          onClick={onSubmit}
-          className="relative z-2 bg-title rounded drop-shadow-shadow text-background font-medium w-full h-s my-2 hover:bg-hover"
-        >
-          저장하기
-        </button>
-      </div>
-    </Layout>
-  );
+          <div className="mb-5">
+            <div className="font-bold text-title">MM 채널 이름 (변경 가능)</div>
+            <div className="flex justify-center">
+              <input
+                onChange={titleChange}
+                type="text"
+                placeholder="ex. 서울_1반_A102"
+                className="w-full text-center placeholder-placeholder border-b-2 border-b-title py-1 px-2 mb-10 placeholder:text-s focus:outline-none focus:border-b-footer"
+              />
+            </div>
+          </div>
+
+          <div className="z-30">
+            <div className="font-bold text-title">달력에 표시할 색상 선택</div>
+            <div className="flex mt-2 mb-12">
+              {/* 색상 선택 */}
+              <div
+                onClick={openColor}
+                style={{ backgroundColor: `${color}` }}
+                className="rounded-full m-[5px] w-[30px] h-[30px] cursor-pointer"
+              ></div>
+              {open ? (
+                <div onClick={openColor} className="absolute z-30">
+                  <ColorPicker color={color} onChange={(color) => setColor(color.hex)} />
+                </div>
+              ) : (
+                <div></div>
+              )}
+              <div onClick={openColor} className="text-xs text-label pt-[9px] ml-1.5 cursor-pointer">
+                {color}
+              </div>
+            </div>
+          </div>
+
+          <button
+            onClick={onSubmit}
+            className="relative z-2 bg-title rounded drop-shadow-shadow text-background font-medium w-full h-s my-2 hover:bg-hover"
+          >
+            저장하기
+          </button>
+        </div>
+      </Layout>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
 export default CreateChannel;
