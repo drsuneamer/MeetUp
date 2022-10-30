d1c8de3fa9462cbd99ea8a5af5415fc47c3469c0
MinjinChae <eominjin25@gmail.com>
2022-10-30 22:15:59 +0900

    [#S07P31A102-127][FE] design: edit font-weight

--- frontend/src/components/modal/WebexModal.tsx
+++ frontend/src/components/modal/WebexModal.tsx
@@ -14,9 +14,9 @@ PropsWithChildren<ModalDefaultType>) {
         <svg onClick={onClickToggleModal} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" className="w-6 h-6 stroke-title mt-[15px] ml-[400px] cursor-pointer">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
   </svg>
-        <div className="flex flex-col justify-center items-center text-l font-bold mt-[60px]">
+        <div className="flex flex-col justify-center items-center mt-[60px]">
           <p className="text-[16px]">https://ssafyclass.webex.com/meet/fusanova</p>
-          <button className="text-[16px] font-bold bg-title text-background hover:bg-hover mt-[40px] rounded w-[350px] h-s drop-shadow-button">이태희(컨설턴트)님의 웹엑스로 이동하기</button>
+          <button className="text-[16px] bg-title text-background hover:bg-hover mt-[40px] rounded w-[350px] h-s drop-shadow-button">이태희(컨설턴트)님의 웹엑스로 이동하기</button>
         </div> 
     </div>
       <div
