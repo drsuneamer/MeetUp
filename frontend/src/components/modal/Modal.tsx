fbb3d9ead2c6799841a3d24bbad806e6a81a6d7d
MinjinChae <eominjin25@gmail.com>
2022-10-30 22:35:25 +0900

    [#][FE] design: edit size of time icon

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -154,7 +154,7 @@ const EventModal = () => {
                     viewBox="0 0 24 24"
                     strokeWidth="2.5"
                     stroke="currentColor"
-                    className="w-7 h-7 ml-[180px]"
+                    className="w-5 h-5 ml-[180px]"
                   >
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
