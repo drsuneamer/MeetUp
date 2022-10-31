585f6c16b335a0871326db91a538c420ff270c91
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-10-31 15:25:27 +0900

    [#S07P31A102-147] feat: meeting 등록 _ 접근 권한 확인 코드 추가

--- backend/src/main/java/com/meetup/backend/service/meeting/MeetingServiceImpl.java
+++ backend/src/main/java/com/meetup/backend/service/meeting/MeetingServiceImpl.java
@@ -3,15 +3,19 @@ package com.meetup.backend.service.meeting;
 import com.meetup.backend.dto.schedule.meeting.MeetingRequestDto;
 import com.meetup.backend.dto.schedule.meeting.MeetingResponseDto;
 import com.meetup.backend.dto.schedule.meeting.MeetingUpdateRequestDto;
+import com.meetup.backend.entity.channel.Channel;
 import com.meetup.backend.entity.meetup.Meetup;
 import com.meetup.backend.entity.schedule.Meeting;
 import com.meetup.backend.entity.user.User;
 import com.meetup.backend.exception.ApiException;
 import com.meetup.backend.exception.ExceptionEnum;
+import com.meetup.backend.repository.channel.ChannelRepository;
+import com.meetup.backend.repository.channel.ChannelUserRepository;
 import com.meetup.backend.repository.schedule.MeetingRepository;
 import com.meetup.backend.repository.meetup.MeetupRepository;
 import com.meetup.backend.repository.schedule.ScheduleRepository;
 import com.meetup.backend.repository.user.UserRepository;
+import com.meetup.backend.service.channel.ChannelUserService;
 import com.meetup.backend.util.converter.StringToLocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +33,9 @@ public class MeetingServiceImpl implements MeetingService {
 
     private final ScheduleRepository scheduleRepository;
 
+    private final ChannelRepository channelRepository;
+
+    private final ChannelUserRepository channelUserRepository;
     private final MeetingRepository meetingRepository;
 
     private final MeetupRepository meetupRepository;
@@ -60,6 +67,10 @@ public class MeetingServiceImpl implements MeetingService {
         String title = meetingRequestDto.getTitle();
         String content = meetingRequestDto.getContent();
         Meetup meetup = meetupRepository.findById(meetingRequestDto.getMeetupId()).orElseThrow(() -> new ApiException(ExceptionEnum.MEETUP_NOT_FOUND));
+        Channel channel = channelRepository.findById(meetup.getChannel().getId()).orElseThrow(() -> new ApiException(ExceptionEnum.CHANNEL_NOT_FOUND));
+
+        if (!channelUserRepository.existsByChannelAndUser(channel, loginUser))
+            throw new ApiException(ExceptionEnum.ACCESS_DENIED);
 
         Meeting meeting = Meeting.builder()
                 .title(title)
