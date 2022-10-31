5f16d57312f5634290e021e6a2fafa09810f664d
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-10-31 14:14:24 +0900

    [#S07P31A102-149] feat: loginUser 스케쥴 반환, 등록, 삭제, 수정 api 컨트롤러 작성

--- backend/src/main/java/com/meetup/backend/controller/ScheduleController.java
+++ backend/src/main/java/com/meetup/backend/controller/ScheduleController.java
@@ -1,19 +1,17 @@
 package com.meetup.backend.controller;
 
-import com.meetup.backend.dto.schedule.AllScheduleRequestDto;
-import com.meetup.backend.dto.schedule.AllScheduleResponseDto;
+import com.meetup.backend.dto.schedule.ScheduleRequestDto;
 import com.meetup.backend.dto.schedule.ScheduleResponseDto;
 import com.meetup.backend.dto.schedule.ScheduleUpdateRequestDto;
 import com.meetup.backend.service.auth.AuthService;
 import com.meetup.backend.service.meeting.ScheduleService;
-import com.meetup.backend.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -28,8 +26,6 @@ import static org.springframework.http.HttpStatus.*;
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
-    private final UserService userService;
-
     private final AuthService authService;
 
     // 스케쥴id로 단일 스케쥴 조회
@@ -42,20 +38,43 @@ public class ScheduleController {
 
     }
 
+    // 로그인 유저의 일정 가져오기
+    @GetMapping("/date/{date}")
+    public ResponseEntity<?> getScheduleResponseDtoByUserAndDate(@PathVariable("date") String date) {
+        log.info("date = {}", date);
+        String userId = authService.getMyInfoSecret().getId();
+        List<ScheduleResponseDto> scheduleResponseDto = scheduleService.getScheduleResponseDtoByUserAndDate(userId, date);
+        return ResponseEntity.status(OK).body(scheduleResponseDto);
+
+    }
+
+
+    // 스케쥴 등록
+    @PostMapping
+    public ResponseEntity<?> createSchedule(@RequestBody @Valid ScheduleRequestDto scheduleRequestDto) {
+        log.info("scheduleRequestDto = {}", scheduleRequestDto);
+        String userId = authService.getMyInfoSecret().getId();
+        Long scheduleId = scheduleService.createSchedule(userId, scheduleRequestDto);
+        return ResponseEntity.status(CREATED).body(scheduleId);
+    }
+
     // 스케쥴 id 해당되는 스케쥴 수정
     @PatchMapping
     public ResponseEntity<?> updateSchedule(@RequestBody @Valid ScheduleUpdateRequestDto scheduleUpdateRequestDto) {
         log.info("scheduleUpdateRequestDto : {}", scheduleUpdateRequestDto);
         String userId = authService.getMyInfoSecret().getId();
-        scheduleService.updateSchedule(userId, scheduleUpdateRequestDto);
+        Long scheduleId = scheduleService.updateSchedule(userId, scheduleUpdateRequestDto);
 
-        return ResponseEntity.status(CREATED).build();
+        return ResponseEntity.status(CREATED).body(scheduleId);
     }
 
-    @GetMapping
-    public ResponseEntity<?> getScheduleByMeetupAndDate(@RequestParam @Valid AllScheduleRequestDto requestDto) {
-        AllScheduleResponseDto result = scheduleService.getScheduleResponseDtoByUserAndDate(authService.getMyInfoSecret().getId(), requestDto.getMeetupId(), requestDto.getDate());
-        return ResponseEntity.status(OK).body(result);
+    @DeleteMapping("/{scheduleId}")
+    public ResponseEntity<?> deleteSchedule(@PathVariable("scheduleId") Long scheduleId) {
+        log.info("scheduleId :{}", scheduleId);
+        String userId = authService.getMyInfoSecret().getId();
+        scheduleService.deleteSchedule(userId, scheduleId);
+
+        return ResponseEntity.status(OK).build();
     }
 
 }
