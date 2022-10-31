a89a07ca5739cec3690ddfae376730d3195c389a
신선영 <sunyeong0412@naver.com>
2022-10-31 17:50:51 +0900

    [#S07P31A102-203][FE] feat: 로그인 여부에 따른 redirect 설정

--- frontend/src/components/auth/axiosConfig.tsx
+++ frontend/src/components/auth/axiosConfig.tsx
@@ -1,16 +1,35 @@
 import axios from 'axios';
+// import { Link } from 'react-router-dom';
 
-const baseURL = 'http://localhost:8080';
 // const baseURL = process.env.REACT_APP_LOCAL_URL; => 왜안되지
-
+const baseURL = 'http://localhost:8080';
 const accessToken = window.localStorage.getItem('accessToken');
 
 // axios를 모듈화하여 이후 baseURL, header를 입력하지 않고 사용 가능
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL,
   headers: {
     Authorization: `Bearer ${accessToken}`,
   },
 });
 
-export default axiosInstance;
+// 인터셉터: axios 요청시마다 로그인 만료 여부 확인
+// axiosInstance.interceptors.request.use((config): any => {
+//   // const accessToken = localStorage.getItem('accessToken');
+//   const tokenExpiresIn = Number(localStorage.getItem('tokenExpiresIn'));
+
+//   const today = new Date();
+//   const parsedToday = today.getTime();
+//   const isExpired = tokenExpiresIn - parsedToday < 0;
+
+//   if (!config.headers) {
+//     config.headers = {};
+//   }
+//   // if (accessToken && !isExpired) {
+//   //   config.headers.Authorization = `Bearer ${accessToken}`;
+//   //   return config;
+//   // }
+//   if (isExpired) {
+//     <Link to="/" />;
+//   }
+// });
---
+++ frontend/src/components/auth/privateRoutes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+
+const PrivateRoutes = () => {
+  //   const accessToken = localStorage.getItem('accessToken');
+  const tokenExpiresIn = Number(localStorage.getItem('tokenExpiresIn'));
+
+  const today = new Date();
+  const parsedToday = today.getTime();
+  const isExpired = tokenExpiresIn - parsedToday < 0;
+  // console.log('is refresh token expired?', isExpired);
+
+  if (isExpired) {
+    return <Navigate to="/login" />;
+  }
+  return <Outlet />;
+};
+
+export default PrivateRoutes;
