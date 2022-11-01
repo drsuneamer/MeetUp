46e92b685fea916e7f0618d92a1cd7bc657a0ab2
신선영 <sunyeong0412@naver.com>
2022-11-01 17:36:32 +0900

    [#S07P31A102-221][FE] feat: edit navigate route

--- frontend/src/components/MeetupList.tsx
+++ frontend/src/components/MeetupList.tsx
@@ -1,12 +1,11 @@
 import MeetupListItem from './MeetupListItem';
 import { tMeetup } from '../types/channels';
-import  Spinner  from './common/Spinner'
+import Spinner from './common/Spinner';
 import { useAppDispatch } from '../stores/ConfigHooks';
 import { useEffect } from 'react';
 import { fetchCalendarList, calendarSelector } from '../stores/modules/meetups';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
 
 function MeetupList() {
   const dispatch = useAppDispatch();
@@ -14,21 +13,21 @@ function MeetupList() {
   const calendar = useSelector(calendarSelector);
 
   useEffect(() => {
-    dispatch(fetchCalendarList())
-    console.log('hi', calendar)
-  }, [])
+    dispatch(fetchCalendarList());
+    console.log('hi', calendar);
+  }, []);
 
   if (!calendar.loading) {
-    return <Spinner />
+    return <Spinner />;
   }
   if (!calendar.calendars) {
-    return null
+    return null;
   }
   return (
     <div className="MeetupList -z-10">
       <h1 className="text-xl font-bold mb-[10px]">참여중인 달력</h1>
       {calendar.calendars.map((value: tMeetup, index: number) => (
-          <MeetupListItem key={value.id} meetup={value} />
+        <MeetupListItem key={index} meetup={value} />
       ))}
     </div>
   );
