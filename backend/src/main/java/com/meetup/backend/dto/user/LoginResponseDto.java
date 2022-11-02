d3556cf4d3faf85c6506702fe4fe17dcdb2b7fd2
seongmin <psm7335@naver.com>
2022-11-02 17:27:59 +0900

    [#]feat: login시 response에 role추가

--- backend/src/main/java/com/meetup/backend/dto/user/LoginResponseDto.java
+++ backend/src/main/java/com/meetup/backend/dto/user/LoginResponseDto.java
@@ -1,12 +1,14 @@
 package com.meetup.backend.dto.user;
 
 import com.meetup.backend.dto.token.TokenDto;
+import com.meetup.backend.entity.user.RoleType;
 import com.meetup.backend.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * created by seongmin on 2022/10/23
+ * updated by seongmin on 2022/11/02
  */
 @Getter
 @AllArgsConstructor
@@ -14,9 +16,10 @@ public class LoginResponseDto {
 
     private String id;
     private String nickname;
+    private RoleType roleType;
     private TokenDto tokenDto;
 
     public static LoginResponseDto of(User user, TokenDto tokenDto) {
-        return new LoginResponseDto(user.getId(), user.getNickname(), tokenDto);
+        return new LoginResponseDto(user.getId(), user.getNickname(), user.getRole(),tokenDto);
     }
 }
