0399199e548db00855d17d56700cab591004dc7e
gyumin.q.lee <gyumin.q.lee@gmail.com>
2022-11-02 13:51:54 +0900

    [#S07P31A102-197][FE] fix: reload page when url changes

--- frontend/src/components/MeetupListItem.tsx
+++ frontend/src/components/MeetupListItem.tsx
@@ -11,10 +11,11 @@ function MeetupListItem(meetup: MeetupListItemProps) {
 
   const OthersCalendar =()=> {
     navigate(`/calendar/${meetup.meetup.id}`)
+    window.location.reload()
   }
 
   return (
-    <div onClick={OthersCalendar} key={meetup.meetup.id} className="MeetupListItem w-full mb-1 drop-shadow-button -z-10">
+    <div onClick={OthersCalendar} key={meetup.meetup.id} className="MeetupListItem w-full mb-1 drop-shadow-button -z-10 cursor-pointer">
       <div className="indexContext bg-offWhite w-full h-[40px] flex flex-wrap">
         <div className="indexLable bg-title w-3/12 h-[40px] flex justify-end ">
           <div className="bg-title mix-blend-multiply w-1/6 h-[40px]" />
