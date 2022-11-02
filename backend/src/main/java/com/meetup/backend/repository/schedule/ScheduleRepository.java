ce9b21fd24e3cb66f24561c12d5449d4b0224261
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-11-02 16:15:50 +0900

    Revert "."
    This reverts commit 1c5d1a4e4748a7c215c49c838163a9b12ff1386a.

--- backend/src/main/java/com/meetup/backend/repository/schedule/ScheduleRepository.java
+++ backend/src/main/java/com/meetup/backend/repository/schedule/ScheduleRepository.java
@@ -14,14 +14,4 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s FROM Schedule s WHERE s.user = :user AND s.type = 'schedule' AND s.start BETWEEN :from AND :to ")
     List<Schedule> findAllByStartBetweenAndUser(LocalDateTime from, LocalDateTime to, User user);
-
-    @Query("SELECT s " +
-            "FROM Schedule s " +
-            "WHERE " +
-            "s.start between :from and :to " +
-            "OR " +
-            "s.end between :from and :to " +
-            "OR " +
-            "(s.start <=:from AND s.start >= :to)")
-    List<Schedule> existsByStartAndEndAndUser(LocalDateTime from, LocalDateTime to, User user);
 }
