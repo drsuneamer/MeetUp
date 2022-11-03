c9405bdc5fcf84dc01ab1bb2b180a3337158a398
신선영 <sunyeong0412@naver.com>
2022-11-03 14:08:16 +0900

    [#S07P31A102-203][FE] bugfix: 로그인 완료 시 개인 캘린더로 redirect

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,8 +4,11 @@ import Image2 from '../assets/background2.png';
 import Image3 from '../assets/background3.png';
 import Image4 from '../assets/background4.png';
 import { useState, useEffect, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const navigate = useNavigate();
+
   // Image[] not working
   const imgArray: any = useMemo(() => [Image1, Image2, Image3, Image4], []);
   const [showImg, setShowImg] = useState();
@@ -14,6 +17,16 @@ function Login() {
     const imgNum: number = Math.round(Math.random() * 3);
     setShowImg(imgArray[imgNum]);
   }, [imgArray]);
+
+  const tokenExpiresIn = Number(localStorage.getItem('tokenExpiresIn'));
+
+  const today = new Date();
+  const parsedToday = today.getTime();
+  const isExpired = tokenExpiresIn - parsedToday < 0;
+
+  if (!isExpired) {
+    navigate(`/calendar/${localStorage.getItem('id')}`);
+  }
 
   return (
     <div className="h-screen bg-cover" style={{ backgroundImage: `url(${showImg})` }}>
