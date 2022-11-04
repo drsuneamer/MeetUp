97ca80e267b5152b22cc5b2c4a4398fd9e209f8a
신선영 <sunyeong0412@naver.com>
2022-11-05 00:47:46 +0900

    [#S07P31A102-237][FE] feat: 웹엑스 모달 기능 완성

--- frontend/src/components/modal/WebexModal.tsx
+++ frontend/src/components/modal/WebexModal.tsx
@@ -1,40 +1,76 @@
-import { PropsWithChildren } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from '../../stores/ConfigHooks';
+import { setWebexModalOpen } from '../../stores/modules/modal';
+import { axiosInstance } from '../auth/axiosConfig';
 
-interface ModalDefaultType {
-  onClickToggleModal: () => void; // 함수 타입 정의
-}
+function WebexModal() {
+  const params = useParams();
+  const dispatch = useAppDispatch();
 
-function WebexModal({ onClickToggleModal }: PropsWithChildren<ModalDefaultType>) {
+  // 모달 온오프
+  const { webexModalIsOpen } = useAppSelector((state) => state.modal);
+  const handleToggleModal = useCallback(() => {
+    dispatch(setWebexModalOpen());
+  }, [dispatch]);
+
+  // webex 주소값, 닉네임 가져오기
+  const [nickname, setNickname] = useState<string>('');
+  const [webex, setWebex] = useState<string>('아직 설정된 주소가 없습니다.');
+
+  useEffect(() => {
+    const userId = params.userId;
+    axiosInstance.get(`/user/webex/${userId}`).then((res) => {
+      if (res.data.webexUrl !== null) {
+        setWebex(res.data.webexUrl);
+      }
+    });
+    axiosInstance.get(`/user/nickname/${userId}`).then((res) => {
+      setNickname(res.data);
+    });
+  });
+
   return (
-    <div className="w-[100%] h-[100%] fixed flex justify-center items-center">
-      <div className="w-[450px] h-[300px] flex flex-col items-center bg-background z-10 rounded drop-shadow-shadow">
-        <svg
-          onClick={onClickToggleModal}
-          xmlns="https://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="2.5"
-          className="w-6 h-6 stroke-title mt-[15px] ml-[400px] cursor-pointer"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-        </svg>
-        <div className="flex flex-col justify-center items-center mt-[60px]">
-          <p className="text-[16px]">https://ssafyclass.webex.com/meet/fusanova</p>
-          <button className="text-[16px] bg-title text-background hover:bg-hover mt-[40px] rounded w-[350px] h-s drop-shadow-button">
-            이태희(컨설턴트)님의 웹엑스로 이동하기
-          </button>
+    <div className={`${webexModalIsOpen ? 'fixed' : 'hidden'} w-[100%] h-[100%] flex justify-center items-center`}>
+      <div className="w-[100%] h-[100%] fixed flex justify-center items-center">
+        <div className="w-[450px] h-[300px] flex flex-col items-center bg-background z-10 rounded drop-shadow-shadow">
+          <svg
+            onClick={handleToggleModal}
+            xmlns="https://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2.5"
+            className="w-6 h-6 stroke-title mt-[15px] ml-[400px] cursor-pointer"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+          <div className="flex flex-col justify-center items-center mt-[60px]">
+            <p className="text-[16px] cursor-default">{webex}</p>
+            {webex !== '아직 설정된 주소가 없습니다.' ? (
+              <button
+                onClick={() => window.open(`${webex}`, '_blank')}
+                className="text-[16px] bg-title text-background hover:bg-hover mt-[40px] rounded w-[350px] h-s drop-shadow-button"
+              >
+                {nickname}의 웹엑스로 이동하기
+              </button>
+            ) : (
+              <button className="text-[16px] bg-footer mt-[40px] rounded w-[350px] h-s drop-shadow-button cursor-default">
+                {nickname}의 웹엑스로 이동하기
+              </button>
+            )}
+          </div>
         </div>
-      </div>
-      <div
-        className="w-[100%] h-[100%] fixed top:0 z-9 bg-[rgba(0,0,0,0.45)]"
-        onClick={(e: React.MouseEvent) => {
-          e.preventDefault();
+        <div
+          className="w-[100%] h-[100%] fixed top:0 z-9 bg-[rgba(0,0,0,0.45)]"
+          onClick={(e: React.MouseEvent) => {
+            e.preventDefault();
 
-          if (onClickToggleModal) {
-            onClickToggleModal();
-          }
-        }}
-      />
+            if (handleToggleModal) {
+              handleToggleModal();
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
