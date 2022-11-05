3f4b49457db8428feb20375ba6cde3e553d73abd
박성민 <psm7335@naver.com>
2022-11-06 01:24:14 +0900

    [#]bugfix: ScheduleRepository @Param 추가

--- backend/src/main/java/com/meetup/backend/repository/schedule/ScheduleRepository.java
+++ backend/src/main/java/com/meetup/backend/repository/schedule/ScheduleRepository.java
@@ -4,15 +4,16 @@ import com.meetup.backend.entity.schedule.Schedule;
 import com.meetup.backend.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 /**
  * created by myeongseok on 2022/10/25
- * updated by myeongseok on 2022/11/01
+ * updated by seongmin on 2022/11/06
  */
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s FROM Schedule s WHERE s.user = :user AND s.start BETWEEN :from AND :to ")
-    List<Schedule> findAllByStartBetweenAndUser(LocalDateTime from, LocalDateTime to, User user);
+    List<Schedule> findAllByStartBetweenAndUser(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to, @Param("user") User user);
 }
