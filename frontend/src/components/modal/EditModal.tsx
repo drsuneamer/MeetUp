254a87c67fad47aba4c51b1afcd86bfd12bd44b8
MinjinChae <eominjin25@gmail.com>
2022-11-09 15:47:47 +0900

    [#][FE] fix: conflict error

--- frontend/src/components/modal/EditModal.tsx
+++ frontend/src/components/modal/EditModal.tsx
@@ -215,7 +215,7 @@ const EditModal = () => {
   // }, [startTimeIndex]);
 
   const handleToggleModal = useCallback(() => {
-    dispatch(setEditModalOpen('close'));
+    dispatch(setEditModalOpen('close''close'));
     window.location.reload();
   }, []);
 
@@ -288,6 +288,11 @@ const EditModal = () => {
   // }
 
   // useEffect(() => {
+<<<<<<< HEAD
+  //   console.log(scheduleDetailId)
+    
+=======
+>>>>>>> 3bcc64839b11f67e81d23c1fd58f682b022123b4
   //   const loadData = async () => {
   //     console.log('hello')
   //     const action = await dispatch(fetchMeetingDetail(scheduleDetailId))
