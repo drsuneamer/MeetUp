7646a4f53d5e3cda30c3557592c398278a2371e6
seongmin <psm7335@naver.com>
2022-11-11 09:56:06 +0900

    [#S07P31A102-283] feat: 팀 멤버 조회 시 닉네임 가져오기 비동기 처리

--- backend/src/main/java/com/meetup/backend/service/AsyncService.java
+++ backend/src/main/java/com/meetup/backend/service/AsyncService.java
@@ -1,15 +1,78 @@
 package com.meetup.backend.service;
 
+import com.meetup.backend.dto.user.UserListInTeamResponseDto;
+import com.meetup.backend.entity.team.TeamUser;
+import com.meetup.backend.entity.user.User;
+import com.meetup.backend.util.converter.JsonConverter;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+import net.bis5.mattermost.client4.MattermostClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * created by seongmin on 2022/11/10
  */
 @Service
+@Slf4j
 public class AsyncService {
     @Async
-    public void run(Runnable runnable) {
-        runnable.run();
+    public CompletableFuture<UserListInTeamResponseDto> getNickname(MattermostClient client, UserListInTeamResponseDto user) {
+        Response response = client.getUser(user.getId()).getRawResponse();
+        JSONObject userObj = new JSONObject();
+        try {
+            userObj = JsonConverter.toJson((BufferedInputStream) response.getEntity());
+
+        } catch (ClassCastException e) {
+            log.error(e.getMessage());
+            log.info("response.getEntity() = {}", response.getEntity());
+            e.printStackTrace();
+        }
+        user.setNickname(userObj.getString("nickname"));
+        log.info("nickname = {}", user.getNickname());
+
+        return CompletableFuture.completedFuture(user);
+    }
+
+    @Async
+    public CompletableFuture<List<UserListInTeamResponseDto>> getResult(MattermostClient client, List<TeamUser> teamUserList) {
+
+        List<UserListInTeamResponseDto> result = new ArrayList<>();
+        for (TeamUser teamUser : teamUserList) {
+            User user = teamUser.getUser();
+            UserListInTeamResponseDto userListInTeamResponseDto = UserListInTeamResponseDto.of(user);
+
+            if (user.getNickname() != null) {
+                continue;
+            }
+
+            Response response = client.getUser(user.getId()).getRawResponse();
+            JSONObject userObj = new JSONObject();
+            try {
+                userObj = JsonConverter.toJson((BufferedInputStream) response.getEntity());
+
+            } catch (ClassCastException e) {
+                log.error(e.getMessage());
+                log.info("response.getEntity() = {}", response.getEntity());
+//                e.printStackTrace();
+            }
+            try {
+                userListInTeamResponseDto.setNickname(userObj.getString("nickname"));
+            } catch (JSONException e) {
+                log.error(e.getMessage());
+//                e.printStackTrace();
+            }
+
+            result.add(userListInTeamResponseDto);
+        }
+        return CompletableFuture.completedFuture(result);
+
     }
 }
